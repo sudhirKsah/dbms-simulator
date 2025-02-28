fb1c419608e3dbdc18d2a8a917e8f3cb8c15febe
elatedvortex <vortexeated@gmail.com>
2025-02-28 19:12:08 +0530

    minor changes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import Link from "next/link";
-
+import FloatingAIAssistant from "./components/FloatingAIAssistant";
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
